62581a4985fa57ee17af76d7badf598758185b54
Siddhi Shah <siddhi117@gmail.com>
2018-11-17 10:59:47 -0600

    Comments added
    Changes push to Health

--- health.py
+++ health.py
@@ -10,22 +10,23 @@ class Health(unittest.TestCase):
         driver = self.driver
 
         driver.get('https://hidden-chamber-33381.herokuapp.com/#/')
-        driver.maximize_window()
+        driver.maximize_window()                                                                      #Maximize the window
         assert 'health' in driver.title
         time.sleep(5)
 
-        #signin = driver.find_element_by_class_name('alert-link')
-        signin = driver.find_element_by_xpath(' //a[@class = \'alert-link\'] ')
+        signin = driver.find_element_by_xpath(' //a[@class = \'alert-link\'] ')                       #Sign in Link
         signin.click()
         time.sleep(3)
 
-        user_name  = driver.find_element_by_id('username')
+        user_name  = driver.find_element_by_id('username')                                            #Username
         user_name.send_keys("admin")
+        time.sleep(2)
 
-        password = driver.find_element_by_id('password')
-        password.send_keys("admin")
+        password = driver.find_element_by_id('password')                                              #Password
+        password.send_keys("admin")                            
+        time.sleep(2)
 
-        submit = driver.find_element_by_xpath(' //button[@class= \'btn btn-primary\'] ')
+        submit = driver.find_element_by_xpath(' //button[@class= \'btn btn-primary\'] ')              #Submit
         submit.click()
         time.sleep(10)
 
